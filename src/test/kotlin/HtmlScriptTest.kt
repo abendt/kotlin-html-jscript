019ed74e57ace0f4d75383c85e674902bf35ce16
Alphonse Bendt <alphonse.bendt@idealo.de>
2018-05-03 18:08:27 +0200

    try some things

--- src/test/kotlin/HtmlScriptTest.kt
+++ src/test/kotlin/HtmlScriptTest.kt
@@ -3,19 +3,51 @@ import kotlinx.html.body
 import kotlinx.html.dom.createHTMLDocument
 import kotlinx.html.dom.serialize
 import kotlinx.html.script
+import kotlinx.html.stream.appendHTML
 import kotlinx.html.unsafe
 import org.junit.Test
 
 class HtmlScriptTest {
-    
+
     @Test
-    fun documentBuilderWithScript() {
+    fun viaStringBuilder() {
+        val sb = StringBuilder()
+        sb.appendHTML(true).body {
+            script {
+                unsafe {
+                    raw("function my() {return 1;}")
+                }
+            }
+        }
+
+        println(sb.toString())
+    }
+
+    @Test
+    fun documentBuilderWithScript1() {
         val html = createHTMLDocument().body {
             script(type = ScriptType.textJScript) {
                 unsafe {
                     raw("""
-                        function my() {return 1;}
-                        """)
+                        |<![CDATA[
+                        | function my() {return 1;}
+                        |]]]]>""".trimMargin())
+                }
+            }
+        }.serialize(true)
+
+        println(html)
+    }
+
+    @Test
+    fun documentBuilderWithScript2() {
+        val html = createHTMLDocument().body {
+            script(type = ScriptType.textJScript) {
+                unsafe {
+                    raw("""
+                        |// <!--
+                        | function my() {return 1;}
+                        |// -->""".trimMargin())
                 }
             }
         }.serialize(true)
